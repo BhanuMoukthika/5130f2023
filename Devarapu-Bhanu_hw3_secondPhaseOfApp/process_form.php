2528dd24537244c4f09688a03a8edc1e71aeb65e
BhanuMoukthika <113474325+BhanuMoukthika@users.noreply.github.com>
2023-10-31 01:40:50 -0400

    process_form.php

--- Devarapu-Bhanu_hw3_secondPhaseOfApp/process_form.php
+++ Devarapu-Bhanu_hw3_secondPhaseOfApp/process_form.php
@@ -1,7 +1,54 @@
 <?php
 // Connect to your database
 $servername = "localhost";
-$username = "root";
+$username = "root";<?php
+// Database configuration
+$servername = "localhost";  
+$username = "root";         
+$password = "";            
+$dbname = "user_data";      
+
+// Create a connection to the database
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Handle form data submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $full_name = $_POST["Full-Name"];
+    $email = $_POST["My-EmailAddress"];
+    $currentlocation = $_POST["Current-location"];
+    $searchonline = $_POST["Search-online"];
+    $desiredlocation = $_POST["Desired-location"];
+    $familybackground = $_POST["family-background"];
+    $hobbies = $_POST["My-Hobbies"];
+    $currentresidence = $_POST["current-residence"];
+    $desiredresidence = $_POST["desired-residence"];
+    $significantother = $_POST["significant-other"];
+    $factsaboutme = $_POST["Facts-AboutMe"];
+    $cvupload = $_POST["cv-upload"];
+    $pictureupload = $_POST["picture-upload"];
+
+    // Insert data into the database
+    $sql = "INSERT INTO user_info (Full_Name,	MyEmail_Address,	Currentlocation,	IAm_From,	Desired_location,	Familybackground,	MyHobbies,	Current_residence,	desiredresidence,	significantother,	FactsAboutMe, cv_upload, picture_upload)
+            VALUES ('$full_name', '$email', '$currentlocation', '$searchonline', '$desiredlocation', '$familybackground', '$hobbies', '$currentresidence', '$desiredresidence', '$significantother', '$factsaboutme','$cvupload','pictureupload')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Form data has been successfully submitted and saved to the database.";
+    } else {
+        
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Close the database connection
+$conn->close();
+?>
+
 $password = "";
 $dbname = "user_data";
 
